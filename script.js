b4913bcc0b82ee6e7b043356ceb054db0f9bfca8
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-03-01 22:23:42 +0530

    question

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//complete this code
+class Rectangle {}
+
+class Square extends Animal {}
+
+// Do not change the code below this line
+window.Rectangle = Rectangle;
+window.Square = Square;
